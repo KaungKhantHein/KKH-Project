fb85a07306d39a91ca024ce77701c0722cf144d8
Kaung Khant Hein <kaungkhanthein051110@gmail.com>
2022-05-04 19:59:16 +0630

    helloooo

--- prj.js
+++ prj.js
@@ -8,7 +8,7 @@ let weather = {
             "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&unit=matric&appid=" + this.apikey
         )
         .then((response)=>response.json())
-        .then((data)=>this.displayWeather(data));   
+        .then((data)=>this.displayWeather(data));    
         
     },
     displayWeather : function (data) {  
@@ -25,5 +25,17 @@ let weather = {
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/h";
         document.querySelector(".pressure").innerText = "Air Pressure: " + pressure + "mb";
+    },
+    search: function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
     }
-}
+
+};
+document.querySelector(".search button").addEventListener("click", function(){
+    weather.search();
+});
+document.querySelector(".search-bar").addEventListener("keyup", function(event){
+    if(event.key == "Enter") {
+        weather.search();
+    }
+});
